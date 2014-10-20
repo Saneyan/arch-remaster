45135fa45059c6bd5621f53a0bf5afa924fda170
TADOKORO Saneyuki <saneyan@mail.gfunction.com>
2014-10-21 00:33:38 +0900

    add help message

--- bin/remaster.sh
+++ bin/remaster.sh
@@ -1,12 +1,48 @@
 #!/bin/sh
 #
-# Remastering Arch Linux ISO.
+# Entry of Arch Remaster.
 #
 # @author   TADOKORO Saneyuki <saneyan@mail.gfunction.com>
 # @license  MIT License
 
+declare -l -r _args=${@:2:($# - 1)}
+
 case $1 in
-  "build") subsets/build ${@:2:($# - 1)};;
-  "deploy") subsets/deploy ${@:2:($# - 1)};;
-  "undeploy") subsets/undeploy ${@:2:($# - 1)};;
+  "build") subsets/build $_args;;
+  "deploy") subsets/deploy $_args;;
+  "undeploy") subsets/undeploy $_args;;
+  *)
+    cat <<_EOT_
+Arch Remaster - Remastering Arch Linux ISO.
+
+./remaster.sh <command> <options>
+
+Commands:
+  build    : Build remastered Arch Linux ISO. 
+  deploy   : Deploy the environment.
+  undeploy : Undeploy the environment. 
+
+Build options:
+  -i : The directory contents.
+  -o : Remastered Arch ISO path.
+  -c : The Cookbook path.
+  -s : Shell in a chroot.
+  -u : Update packages and the system.
+  -f : Enable full automatic building.
+  -g : Generate a new ISO file.
+  -q : Squash a filesystem.
+  -a : Specify system architecture.
+
+Deploy options:
+  -i : Input Arch Linux ISO.
+  -d : Destination directory.
+  -a : Specify system architecture.
+
+Undeploy options:
+  -d : The directory contents.
+  -a : Specify system architecture.
+  -X : Remove all contents.
+
+_EOT_
+    ;;
 esac
