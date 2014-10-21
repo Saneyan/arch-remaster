022182b11ceeb0bc211c57ed98604a09ed6d9c89
TADOKORO Saneyuki <saneyan@mail.gfunction.com>
2014-10-21 16:51:38 +0900

    fix cookbook settings

--- chroot/initialize.zsh
+++ chroot/initialize.zsh
@@ -14,7 +14,9 @@ if [ -e /cookbooks ]; then
   export PATH=$PATH:$(ruby -e "print Gem.user_dir")/bin
   gem install chef berkshelf --verbose
   if [ $? = 0 ]; then
-    cd cookbooks/$COOKBOOK
+    cd /cookbooks/$COOKBOOK
     berks install
+    berks vendor chef/cookbooks
+    chef-solo -c ./solo.rb -j ./solo.json install
   fi
 fi
--- chroot/update.zsh
+++ chroot/update.zsh
@@ -1,10 +1,10 @@
 echo "==> Update packages"
 yaourt -Syua --force --noconfirm archiso linux
 
-mkinitcpio -p linux
-
-if [ -e /cookbooks ]; then
+if [[ -n $COOKBOOK ]] && [ -e /cookbooks ]; then
   export PATH=$PATH:$(ruby -e "print Gem.user_dir")/bin
   cd cookbooks/$COOKBOOK
-  berks update
+  berks install
+  [ ! -e chef/cookbooks ] && berks vendor chef/cookbooks
+  chef-solo -c ./solo.rb -j ./solo.json install
 fi
